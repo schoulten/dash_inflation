ee48ccfb8edcc24840dd9b24fe965ca573d74f52
Fernando da Silva <fernando.schoulten@hotmail.com>
2021-12-20 16:21:06 -0300

    [feat] Add data ETL script

---
+++ R/data_etl.R
@@ -0,0 +1,48 @@
+
+# Pacotes -----------------------------------------------------------------
+
+library(GetBCBData)
+library(magrittr)
+library(dplyr)
+library(readr)
+
+
+# Importar dados ----------------------------------------------------------
+
+# Variáveis a serem importadas
+vars_inflation <- c(
+  "IPCA"    = 433,
+  "IGP-M"   = 189,
+  "IGP-DI"  = 190,
+  "INPC"    = 188,
+  "IPC-Br"  = 191
+  )
+
+# Importar dados
+raw_data <- GetBCBData::gbcbd_get_series(
+  id          = vars_inflation,
+  first.date  = as.Date("2000-01-01"),
+  format.data = "long",
+  use.memoise = FALSE
+  )
+
+# Verificar se há dados faltando
+if(dplyr::n_distinct(raw_data$series.name) != length(vars_inflation)){
+  stop("Missing variables, please check ETL process.")
+}
+
+
+# Tratar dados ------------------------------------------------------------
+
+tbl_inflation <- raw_data %>%
+  dplyr::as_tibble() %>%
+  dplyr::select(
+    "date"     = "ref.date",
+    "variable" = "series.name",
+    "mom"      = "value"
+    )
+
+
+# Salvar dados ------------------------------------------------------------
+
+readr::write_rds(x = tbl_inflation, file = "data/tbl_inflation.rds")
